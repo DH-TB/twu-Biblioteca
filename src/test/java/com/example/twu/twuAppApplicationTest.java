f928534b474a884a769af9822761a4f9f01050b7
DH-TB <929659475@qq.com>
2018-07-13 18:03:32 +0800

    finish get book list test
    Signed-off-by: DH-TB <929659475@qq.com>

--- src/test/java/com/example/twu/twuAppApplicationTest.java
+++ src/test/java/com/example/twu/twuAppApplicationTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class twuAppApplicationTest {
-
     twuAppApplication biblioteca;
     Menu menu;
 
@@ -19,9 +18,35 @@ public class twuAppApplicationTest {
 
     @Test
     public void should_return_welcome_info() {
-        String welcomeMessage = menu.getWelcomeInfo();
+        String result = menu.getWelcomeInfo();
         String expected = "*********** Welcome to Biblioteca! ***********";
-        assertEquals(welcomeMessage, expected);
+        assertEquals(result, expected);
     }
+
+    @Test
+    public void should_return_menu() {
+        String result = menu.getMenu();
+        String expected = "\n***********\n" +
+                "1 show all book\n" +
+                "2 checkout book\n" +
+                "3 return book\n" +
+                "4 exit\n" +
+                "***********\n"+
+                "please choose number you want：\n";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void should_return_book_list_when_input_1() {
+        BookController.saveBookList();
+        String result = BookController.getBookInfoList();
+        String expected = "1 | Java | TWU | 2018/3/16 | 人民邮电出版社\n" +
+                "2 | TDD | TWU | 2018/3/16 | 人民邮电出版社\n" +
+                "3 | Refactor | TWU | 2018/3/16 | 人民邮电出版社\n" +
+                "4 | Pair Programing | TWU | 2018/3/16 | 人民邮电出版社\n" +
+                "5 | Code Small | TWU | 2018/3/16 | 人民邮电出版社\n";
+        assertEquals(result, expected);
+    }
+
 
 }
